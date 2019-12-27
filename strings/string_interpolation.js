e92561d4d683036059bba41818bca6238c26f835
Tesfaye Belachew Abebe <get.tesfaye.belachew@gmail.com>
2019-12-27 20:28:22 +0300

    String interpolation, trimming, and padding practice

---
+++ strings/string_interpolation.js
@@ -0,0 +1,9 @@
+/**
+ * Learning string interpolation aka template string
+ */
+
+ let firstName = "Tesfaye", lastName ="Belachew"
+ let fullName = `Your full name: ${firstName} ${lastName}`
+ console.log(fullName)//Your full name: Tesfaye Belachew
+ 
+ 
---
+++ strings/string_padding_and_trimming.js
@@ -0,0 +1,45 @@
+let str = "Hello"
+//left start padding
+console.log("str.padStart(5) =>",str.padStart(5))
+console.log("str.padStart(8) =>",str.padStart(8))
+console.log("str.padStart(8,'*') =>",str.padStart(8,'*'))
+console.log("str.padStart(8,'12345') =>",str.padStart(8,'12345'))
+console.log("str.padStart(8,'ab') =>",str.padStart(8,'ab'))
+/**
+ * Result:
+ * str.padStart(5) => Hello
+str.padStart(8) =>    Hello
+str.padStart(8,'*') => ***Hello
+str.padStart(8,'12345') => 123Hello
+str.padStart(8,'ab') => abaHello
+ */
+
+ 
+//Right end padding
+console.log("\nstr.padEnd(5) =>",str.padEnd(5))
+console.log("str.padEnd(8) =>",str.padEnd(8))
+console.log("str.padEnd(8,'*') =>",str.padEnd(8,'*'))
+console.log("str.padEnd(8,'12345') =>",str.padEnd(8,'12345'))
+console.log("str.padEnd(8,'ab') =>",str.padEnd(8,'ab'))
+/**
+ * Result:
+ * str.padEnd(5) => Hello
+str.padEnd(8) => Hello
+str.padEnd(8,'*') => Hello***
+str.padEnd(8,'12345') => Hello123
+str.padEnd(8,'ab') => Helloaba
+ */
+
+ //Using trim methods: to remove unecessary spaces from a string
+
+ str = "   some stuff   \t\t"
+ console.log("\nstr.trim() =>",str.trim())
+ console.log("str.trimStart() =>",str.trimStart())
+ console.log("str.trimEnd() =>",str.trimEnd())
+ /**
+  * Result: 
+  * str.trim() => some stuff
+str.trimStart() => some stuff
+str.trimEnd() =>    some stuff
+  */
+
