c4341f711becccd7fc052e3318b7b3bb504d924c
Tesfaye Belachew Abebe <get.tesfaye.belachew@gmail.com>
2020-01-02 10:47:00 +0300

    solved oop csbin challenges and their extension up to EXTENSION: Mixins

--- JS  the hard parts/OOP/oop_csbin_challenges.js
+++ JS  the hard parts/OOP/oop_csbin_challenges.js
@@ -21,7 +21,7 @@ Create a function that accepts two inputs (name and age) and returns an object. 
     3. add an age property to the newly created object with its value being the 'age' argument passed into the function
     4. return the objec
  * ***/
-
+let { log: print, info: showMsg } = console;
 function makePerson(name, age) {
   // add code here
   this.name = name;
@@ -32,8 +32,8 @@ function makePerson(name, age) {
 const vicky = makePerson("Vicky", 24);
 
 // /********* Uncomment these lines to test your work! *********/
-console.log(vicky.name); // -> Logs 'Vicky'
-console.log(vicky.age); // -> Logs 24
+showMsg(vicky.name); // -> Logs 'Vicky'
+showMsg(vicky.age); // -> Logs 24
 
 /****************************************************************
                        USING OBJECT.CREATE
@@ -47,7 +47,7 @@ Inside personStore object, create a property greet where the value is a function
 const personStore = {
   // add code here
   greet :function () {
-     console.log('hello')
+     showMsg('hello')
      return;
   }
 };
@@ -64,7 +64,7 @@ function personFromPersonStore(name, age) {
   this.name=name;
   this.age=age;
   this.greet=function () {
-    console.log(`hello`)
+    showMsg(`hello`)
 }
   return Object.create(this) 
 }
@@ -72,8 +72,8 @@ function personFromPersonStore(name, age) {
 const sandra = personFromPersonStore("Sandra", 26);
 
 // /********* Uncomment these lines to test your work! *********/
-console.log(sandra.name); // -> Logs 'Sandra'
-console.log(sandra.age); //-> Logs 26
+showMsg(sandra.name); // -> Logs 'Sandra'
+showMsg(sandra.age); //-> Logs 26
 sandra.greet(); //-> Logs 'hello'
 
 /*** hallenge 3/3
@@ -81,7 +81,7 @@ Without editing the code you've already written, add an introduce method to the 
  ***/
 // add code here
 sandra.introduce =function () {
-    console.log(`Hi, my name is [${this.name}]`)
+    console.log(`Hi, my name is ${this.name}`)
 }
 
 
@@ -91,68 +91,110 @@ sandra.introduce(); // -> Logs 'Hi, my name is Sandra'
                     USING THE 'NEW' KEYWORD
 ****************************************************************/
 
-/*** CHALLENGE 1 of 3 ***/
+/*** CHALLENGE 1 of 3 
+ * Using the NEW keyword 
+Create a function PersonConstructor that uses the this keyword to save a single property onto its scope called greet. greet should be a function that logs the string 'hello'. 
+ * ***/
 
 function PersonConstructor() {
   // add code here
+  this.greet = function () {
+    showMsg('hello')
+  }
 }
 
 // /********* Uncomment this line to test your work! *********/
 const simon = new PersonConstructor();
-// simon.greet(); // -> Logs 'hello'
+simon.greet(); // -> Logs 'hello'
 
-/*** CHALLENGE 2 of 3 ***/
+/*** CHALLENGE 2 of 3 
+ * Challenge 2/3
+Create a function personFromConstructor that takes as input a name and an age. When called, the function will create person objects using the new keyword instead of the Object.create method. 
+ * ***/
 
 function personFromConstructor(name, age) {
   // add code here
+  this.name =name;
+  this.age =age;
+  this.greet = function () {
+    showMsg('hello')
+  }
+  return this;
 }
 
-const mike = personFromConstructor("Mike", 30);
+const mike = new personFromConstructor("Mike", 30);
 
 // /********* Uncomment these lines to test your work! *********/
-// console.log(mike.name); // -> Logs 'Mike'
-// console.log(mike.age); //-> Logs 30
-// mike.greet(); //-> Logs 'hello'
+showMsg(mike.name); // -> Logs 'Mike'
+showMsg(mike.age); //-> Logs 30
+mike.greet(); //-> Logs 'hello'
 
-/*** CHALLENGE 3 of 3 ***/
+/*** CHALLENGE 3 of 3 
+ * 
+ * Challenge 3/3
+Without editing the code you've already written, add an introduce method to the PersonConstructor function that logs "Hi, my name is [name]". 
+ * ***/
 // add code here
+mike.introduce = function () {
+  showMsg(`Hi, my name is ${this.name}`)
+}
 
-// mike.introduce(); // -> Logs 'Hi, my name is Mike'
+mike.introduce(); // -> Logs 'Hi, my name is Mike'
 
 /****************************************************************
                         USING ES6 CLASSES
 ****************************************************************/
 
-/*** CHALLENGE 1 of 3 ***/
+/*** 
+ * Challenge 1/2
+Create a class PersonClass. PersonClass should have a constructor that is passed an input of name and saves it to a property by the same name. PersonClass should also have a method called greet that logs the string 'hello'. 
+ * ***/
 
 class PersonClass {
-  constructor() {
+  constructor(name) {
     // add code here
+    this.name=name;
   }
 
   // add code here
+  greet(){
+    showMsg(`hello`)
+  } 
 }
 
 // /********* Uncomment this line to test your work! *********/
 const george = new PersonClass();
-// george.greet(); // -> Logs 'hello'
+george.greet(); // -> Logs 'hello'
 
-/*** CHALLENGE 2 of 3 ***/
+/*** 
+ * Challenge 2/2
+Create a class DeveloperClass that creates objects by extending the PersonClass class. In addition to having a name property and greet method, DeveloperClass should have an introduce method. When called, introduce should log the string 'Hello World, my name is [name]'. 
+ * ***/
 
-// add code here
+// add code here 
+class DeveloperClass extends PersonClass {
+     introduce (){
+     showMsg(`Hello World, my name is ${this.name}`)
+   }
+
+}
 
 // /********* Uncomment these lines to test your work! *********/
-// const thai = new DeveloperClass('Thai', 32);
-// console.log(thai.name); // -> Logs 'Thai'
-// thai.introduce(); //-> Logs 'Hello World, my name is Thai'
+const thai = new DeveloperClass('Thai', 32);
+showMsg(thai.name); // -> Logs 'Thai'
+thai.introduce(); //-> Logs 'Hello World, my name is Thai'
 
 /****************************************************************
                       EXTENSION: SUBCLASSING
 ****************************************************************/
+/**
+ * Challenge 1/5
+Create an object adminFunctionStore that has access to all methods in the userFunctionStore object, without copying them over individually. 
+ */
 
 const userFunctionStore = {
   sayType: function() {
-    console.log("I am a " + this.type);
+    showMsg("I am a " + this.type);
   }
 };
 
@@ -163,20 +205,30 @@ function userFactory(name, score) {
   user.score = score;
   return user;
 }
-
-// const adminFunctionStore /* Put code here */ ;
-
+//solution
+const adminFunctionStore = new userFactory('tesfanur',8);
+showMsg({adminFunctionStore})
+/**
+ * 
+ Challenge 2/5
+Create an adminFactory function that creates an object with all the same data fields (and default values) as objects of the userFactory class, but without copying each data field individually. 
+ */
 function adminFactory(name, score) {
   // Put code here
+  return userFactory(name,score)
+  
+}
+//test adminFactory
+showMsg({adminFactory: adminFactory('tesfanur',12)})
+/* Put code here for a method called sharePublicMessage*/
+const adminFromFactory = new adminFactory("Eva", 5);
+adminFromFactory.sharePublicMessage = function () {
+  showMsg(`Welcome users!`)
 }
 
-/* Put code here for a method called sharePublicMessage*/
-
-const adminFromFactory = adminFactory("Eva", 5);
-
 // /********* Uncomment these lines to test your work! *********/
-// adminFromFactory.sayType() // -> Logs "I am a Admin"
-// adminFromFactory.sharePublicMessage() // -> Logs "Welcome users!"
+adminFromFactory.sayType() // -> Logs "I am a Admin"
+adminFromFactory.sharePublicMessage() // -> Logs "Welcome users!"
 
 /****************************************************************
 EXTENSION: MIXINS
@@ -187,14 +239,14 @@ class Dog {
     this.legs = 4;
   }
   speak() {
-    console.log("Woof!");
+    showMsg("Woof!");
   }
 }
 
 const robotMixin = {
   skin: "metal",
   speak: function() {
-    console.log(`I have ${this.legs} legs and am made of ${this.skin}`);
+    showMsg(`I have ${this.legs} legs and am made of ${this.skin}`);
   }
 };
 
