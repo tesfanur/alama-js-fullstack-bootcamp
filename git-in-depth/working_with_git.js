ce7950f063da696fc8381465554c41a0de0c68ad
Tesfaye Belachew Abebe <get.tesfaye.belachew@gmail.com>
2020-01-05 10:57:56 +0300

    added welcomeToGit function

---
+++ git-in-depth/working_with_git.js
@@ -0,0 +1,17 @@
+/**
+ * Trainee: Tesfaye Belachew
+ * Email: get.tesfaye.belachew@gmail.com
+ * Remote Self Teaching Alama JS Full Stack Training Bootcamp
+ * From Dec 14, 2019 to Feb 29, 2020
+ * Date: Jan 05, 2020
+ * Addis Ababa, Ethiopia
+ */
+
+//write some code
+let { log: showMsg } = console;
+function welcomeToGit(name) {
+  return `Hello ${name}!
+Wlecome to study the magical tool called git VCS!`;
+}
+
+showMsg(welcomeToGit("Tesfanur"));
